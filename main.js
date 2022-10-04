2f0828c5310f150d052f59651a83a77a255a85c2
Alejandro Murillo <alejandromurillin@gmail.com>
2022-10-04 17:26:15 -0500

    fusionamos el menu mobile y responsive

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const menuEmail = document.querySelector(".navbar-email")
 const DesktopMenu = document.querySelector(".desktop-menu")
+const menuHamIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+menuHamIcon.addEventListener("click", togglemobileMenu)
+
 
 function toggleDesktopMenu() {
     DesktopMenu.classList.toggle("inactive")
+}
+
+function togglemobileMenu() {
+    mobileMenu.classList.toggle("inactive")
+
 }
